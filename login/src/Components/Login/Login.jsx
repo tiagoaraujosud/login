4786b296ba019c5ff2a3618d975a85ffe717c153
tiagoaraujosud <tiagoaraujosud@gmail.com>
2024-05-16 08:22:13 -0300

    React Hooks UseState

--- login/src/Components/Login/Login.jsx
+++ login/src/Components/Login/Login.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import {FaUser, FaLock} from 'react-icons/fa';
 
+import { useState } from "react";
+
+import "./Login.css";
+
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
         <div className="container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Login Page</h1>
                 <div>
-                    <input type="email" placeholder="E-mail" />
+                    <input type="email" placeholder="E-mail" onChange={(e) => setUsername(e.target.value)}/>
                     <FaUser />
                 </div>
                 <div>
-                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
                     <FaLock />
                 </div>
 
